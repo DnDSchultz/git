ff35ee72cd96e92d0cfa2984d9431c54b72b0883
Darrin Schultz <dndschultz@yahoo.com>
2019-01-20 18:34:49 -0600

    Added application.py and index.html to git

---
+++ application.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+
+@app.route("/")
+@app.route("/index")
+def index():
+   print("Py")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
